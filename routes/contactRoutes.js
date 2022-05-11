c6d6927053ea35c0fae431b7f1163d6b61c4a9f4
makena <mduncan18@byui.edu>
2022-05-10 21:55:40 -0600

    commited

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,5 +1,10 @@
 const routes = require('express').Router();
 const contactController = require('../controllers/contactController');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 routes.get('/', contactController.getContacts);
 routes.get('/:id', contactController.getContact);
