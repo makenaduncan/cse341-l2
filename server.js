7ec1b85cc28385f364faaf9c8e7e7f2bb7f72c86
makena <mduncan18@byui.edu>
2022-05-11 08:14:40 -0600

    I hope it's the last one

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const contactRoutes = require('./routes/contactRoutes');
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
   next();
 });
 app.use('/contacts', contactRoutes);
